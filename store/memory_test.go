a6715806e4aa3c3a4841256741587e0299cb9e32
Eiswuerfel13 <erikmue13@gmail.com>
2025-02-01 22:33:43 +0100

    added SaveEvent test coverage

--- store/memory_test.go
+++ store/memory_test.go
@@ -1,18 +1,87 @@
 package store_test
 
 import (
+	"sync"
 	"testing"
+
+	"github.com/FlauschigDings/lavender"
+	"github.com/FlauschigDings/lavender/example"
+	"github.com/FlauschigDings/lavender/store"
 )
 
 func TestSaveEvent(t *testing.T) {
 	t.Run("single", func(t *testing.T) {
+		memStore := store.NewInMemoryStore()
+		err := memStore.SaveEvents(example.New(), []lavender.Event{
+			&example.Create{
+				User: *example.NewUser("Nils0", "dasIstMeinPassword,Ja das ist toll"),
+			},
+		})
+		if err != nil {
+			t.Error(err)
+		}
 	})
 
 	t.Run("multi", func(t *testing.T) {
+		memStore := store.NewInMemoryStore()
+
+		err := memStore.SaveEvents(example.New(), []lavender.Event{
+			&example.Create{
+				User: *example.NewUser("Nils1", "dasIstMeinPassword,Ja das ist toll"),
+			},
+			&example.Create{
+				User: *example.NewUser("Nils1", "dasIstMeinPassword,Ja das ist toll"),
+			},
+			&example.Create{
+				User: *example.NewUser("Nils1", "dasIstMeinPassword,Ja das ist toll"),
+			},
+			&example.Create{
+				User: *example.NewUser("Nils1", "dasIstMeinPassword,Ja das ist toll"),
+			},
+			&example.Create{
+				User: *example.NewUser("Nils1", "dasIstMeinPassword,Ja das ist toll"),
+			},
+			&example.Create{
+				User: *example.NewUser("Nils1", "dasIstMeinPassword,Ja das ist toll"),
+			},
+			&example.Create{
+				User: *example.NewUser("Nils1", "dasIstMeinPassword,Ja das ist toll"),
+			},
+
+			&example.Create{
+				User: *example.NewUser("Nils1", "dasIstMeinPassword,Ja das ist toll"),
+			},
+			&example.Create{
+				User: *example.NewUser("Nils1", "dasIstMeinPassword,Ja das ist toll"),
+			},
+		})
+		if err != nil {
+			t.Error(err)
+		}
 	})
 
 	t.Run("multi-thread", func(t *testing.T) {
+		memoryStore := store.NewInMemoryStore()
+		var wg sync.WaitGroup
 
+		for i := 0; i < 10000; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				memoryStore.SaveEvents(example.New(), []lavender.Event{
+					&example.Create{
+						User: *example.NewUser("Nils2", "dasIstMeinPassword,Ja das ist toll"),
+					},
+				})
+				events, err := memoryStore.LoadEvents(example.New())
+				if err != nil {
+					t.Error(err)
+				}
+				_ = events
+			}()
+		}
+
+		wg.Wait()
 	})
 }
 
